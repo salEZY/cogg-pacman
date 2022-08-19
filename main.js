26a643b193e6ae285ce00807a059e8742fdb9d91
salEZY <bobicaleks@yahoo.com>
2022-08-19 14:03:32 +0200

    move ghosts

--- main.js
+++ main.js
@@ -7,16 +7,106 @@ let score = 0
 let scoreToBeat = 0
 let lives = 3
 let level = 1
+let playerX = 30
+let playerY = 30
+let ghostCropY = 0
 let keyclick = {}
 let pills = []
 let ghosts = []
 const player = {
-    x: 30,
-    y: 30,
+    x: playerX,
+    y: playerY,
     pacMouth: 320,
     pacDir: 0,
     pSize: 32,
     speed: 10
+}
+
+
+
+const collisionHandler = (player) => {
+    // Border collision
+    if (player.x >= (canvas.width - 58)) {
+        player.x = canvas.width - 58;
+    }
+
+
+    if (player.y >= (canvas.height - 58)) {
+        // PORTAL
+        player.y += player.speed;
+        if ((player.x > 165 && player.x < 210) || (player.x > 510 && player.x < 610)) {
+            if (player.y > canvas.height) {
+                player.x = player.x < 210 ? 580 : 180
+                player.y = canvas.height - 50
+                player.pacDir = 96
+            }
+        }
+        else {
+            player.y = canvas.height - 58;
+        }
+    }
+
+    if (player.x < 10) {
+        player.x = 10;
+    }
+    if (player.y < 10) {
+        player.y = 10;
+    }
+
+    // Maze collision
+    if ((player.x > 345 && player.x < 360) && player.y < 100 && keyclick.ArrowRight) player.x -= player.speed
+    if ((player.x > 400 && player.x < 410) && player.y < 100 && keyclick.ArrowLeft) player.x += player.speed
+    if ((player.x > 350 && player.x < 398) && (player.y >= 89 && player.y < 115) && keyclick.ArrowUp) player.y += player.speed
+    if ((player.x > 35 && player.x < 105) && (player.y > 155 && player.y < 495)) {
+        if (keyclick.ArrowRight) player.x -= player.speed
+        if (keyclick.ArrowLeft) player.x += player.speed
+    }
+    if ((player.x >= 50 && player.x < 95)) {
+        if (player.y > 50 && player.y < 100) player.x -= player.speed
+        if (player.y > 145 && player.y < 160) player.y -= player.speed
+        if ((player.y > 480 && player.y < 520) && keyclick.ArrowUp) player.y += player.speed
+    }
+
+    if (player.x >= 300 && player.x < 325) {
+        if ((player.y > 55 && player.y < 100) || (player.y > 165 && player.y < 490)) player.x += player.speed
+    }
+    if ((player.x > 130 && player.x < 145) && (player.y > 155 && player.y < 340)) player.x -= player.speed
+    if (player.x > 155 && player.x < 310) {
+        if (player.y > 145 && player.y < 160) player.y -= player.speed
+        if (player.y > 340 && player.y < 355) player.y = player.y += player.speed
+    }
+    if ((player.x > 245 && player.x < 270) && (player.y > 350 && player.y < 480)) player.x -= player.speed
+    if ((player.x > 260 && player.x < 315) && (player.y > 470 && player.y < 500)) player.y += player.speed
+    if ((player.y > 35 && player.y < 55) || (player.y > 90 && player.y < 110)) {
+        if ((player.x > 50 && player.x < 300) || (player.x >= 450 && player.x < 705)) {
+            if (keyclick.ArrowUp) player.y += player.speed
+            if (keyclick.ArrowDown) player.y -= player.speed
+            if (keyclick.ArrowLeft) player.x -= player.speed
+        }
+    }
+    if ((player.y > 55 && player.y < 100) && (player.x > 435 && player.x < 705)) {
+        if (keyclick.ArrowRight) player.x -= player.speed
+        if (keyclick.ArrowLeft) player.x += player.speed
+    }
+
+    if (player.y > 170 && player.y < 480) {
+        if ((player.x >= 710 && player.x < 725) || (player.x >= 520 && player.x < 540)) player.x += player.speed
+        if ((player.x >= 545 && player.x < 560) || (player.x > 345 && player.x < 360)) player.x -= player.speed
+    }
+    if ((player.x > 355 && player.x < 525) || (player.x > 550 && player.x < 720)) {
+        if (player.y > 475 && player.y < 500) player.y += player.speed
+        if (player.y > 160 && player.y < 170) player.y -= player.speed
+    }
+}
+
+const ghostMove = (ghost) => {
+
+    const direction = Math.floor(Math.random() * 4) + 1
+
+    if (direction === 1) ghost.y -= ghost.speed
+    if (direction === 2) ghost.x += ghost.speed
+    if (direction === 3) ghost.y += ghost.speed
+    if (direction === 4) ghost.x -= ghost.speed
 }
 
 // Ghost array
@@ -24,7 +114,7 @@ const createGhostObjs = (ctx, characters) => {
     for (let i = 0; i < level + 1; i++) {
         let ghostY = 290 + (80 * i)
         let ghostCropX = 0 + (65 * i)
-        ghosts.push({ x: 540, y: ghostY, speed: 8, eatable: false, ghostCropX, ghostCropY: 0, sWidth: 32, sHeight: 32, dWidth: 45, dHeight: 45 })
+        ghosts.push({ x: 540, y: ghostY, speed: 7, eatable: false, ghostCropX, ghostCropY, sWidth: 32, sHeight: 32, dWidth: 45, dHeight: 45, ghost: true })
 
     }
 }
@@ -97,7 +187,7 @@ document.addEventListener('keydown', function (event) {
 document.addEventListener('keyup', function (event) {
     delete keyclick[event.code];
 }, false);
-console.log(ghosts)
+
 const move = (keyclick) => {
     // Moving
     if (keyclick.ArrowLeft) {
@@ -120,89 +210,31 @@ const move = (keyclick) => {
         player.speed = 0
     }
 
-
-    // Border collision
-    if (player.x >= (canvas.width - 58)) {
-        player.x = canvas.width - 58;
-    }
-    console.log(player.x, player.y)
-    console.log(ghosts)
-    if (player.y >= (canvas.height - 58)) {
-        // PORTAL
-        player.y += player.speed;
-        if ((player.x > 165 && player.x < 210) || (player.x > 510 && player.x < 610)) {
-            if (player.y > canvas.height) {
-                player.x = player.x < 210 ? 580 : 180
-                player.y = canvas.height - 50
-                player.pacDir = 96
-            }
-        }
-        else {
-            player.y = canvas.height - 58;
-        }
-    }
-
-    if (player.x < 10) {
-        player.x = 10;
-    }
-    if (player.y < 10) {
-        player.y = 10;
-    }
-
-    // Maze collision
-    if ((player.x > 345 && player.x < 360) && player.y < 100 && keyclick.ArrowRight) player.x -= player.speed
-    if ((player.x > 400 && player.x < 410) && player.y < 100 && keyclick.ArrowLeft) player.x += player.speed
-    if ((player.x > 350 && player.x < 398) && (player.y >= 89 && player.y < 115) && keyclick.ArrowUp) player.y += player.speed
-    if ((player.x > 35 && player.x < 105) && (player.y > 155 && player.y < 495)) {
-        if (keyclick.ArrowRight) player.x -= player.speed
-        if (keyclick.ArrowLeft) player.x += player.speed
-    }
-    if ((player.x >= 50 && player.x < 95)) {
-        if (player.y > 50 && player.y < 100) player.x -= player.speed
-        if (player.y > 145 && player.y < 160) player.y -= player.speed
-        if ((player.y > 480 && player.y < 520) && keyclick.ArrowUp) player.y += player.speed
-    }
-
-    if (player.x >= 300 && player.x < 325) {
-        if ((player.y > 55 && player.y < 100) || (player.y > 165 && player.y < 490)) player.x += player.speed
-    }
-    if ((player.x > 130 && player.x < 145) && (player.y > 155 && player.y < 340)) player.x -= player.speed
-    if (player.x > 155 && player.x < 310) {
-        if (player.y > 145 && player.y < 160) player.y -= player.speed
-        if (player.y > 340 && player.y < 355) player.y = player.y += player.speed
-    }
-    if ((player.x > 245 && player.x < 270) && (player.y > 350 && player.y < 480)) player.x -= player.speed
-    if ((player.x > 260 && player.x < 315) && (player.y > 470 && player.y < 500)) player.y += player.speed
-    if ((player.y > 35 && player.y < 55) || (player.y > 90 && player.y < 110)) {
-        if ((player.x > 50 && player.x < 300) || (player.x >= 450 && player.x < 705)) {
-            if (keyclick.ArrowUp) player.y += player.speed
-            if (keyclick.ArrowDown) player.y -= player.speed
-            if (keyclick.ArrowLeft) player.x -= player.speed
-        }
-    }
-    if ((player.y > 55 && player.y < 100) && (player.x > 435 && player.x < 705)) {
-        if (keyclick.ArrowRight) player.x -= player.speed
-        if (keyclick.ArrowLeft) player.x += player.speed
-    }
-
-    if (player.y > 170 && player.y < 480) {
-        if ((player.x >= 710 && player.x < 725) || (player.x >= 520 && player.x < 540)) player.x += player.speed
-        if ((player.x >= 545 && player.x < 560) || (player.x > 345 && player.x < 360)) player.x -= player.speed
-    }
-    if ((player.x > 355 && player.x < 525) || (player.x > 550 && player.x < 720)) {
-        if (player.y > 475 && player.y < 500) player.y += player.speed
-        if (player.y > 160 && player.y < 170) player.y -= player.speed
-    }
-
+    // Player collision
+    collisionHandler(player)
 
     // Pill detection
     pills.map(pill => {
         if (player.x <= pill.x && pill.x <= (player.x + 32) && player.y <= pill.y && pill.y <= (player.y + 32)) {
             pill.x = -20
             pill.y = -20
-            pill.superPill ? score += 100 : score += 10
+            if (pill.superPill) {
+                score += 100
+                ghostCropY = 32
+            } else
+                score += 10
         }
     })
+
+    // Ghost collision
+    ghosts.map(ghost => {
+        if (player.x <= (ghost.x + 26) && ghost.x <= (player.x + 26) && player.y <= (ghost.y + 26) && ghost.y <= (player.y + 32)) {
+            lives -= 1
+            player.x = playerX;
+            player.y = playerY;
+        }
+    })
+
 
     // Open/closed mouth
     if (player.pacMouth == 320) {
@@ -230,20 +262,7 @@ const render = () => {
 
     drawPills(ctx, pills)
 
-    // Next level
-    if (score === scoreToBeat) {
-        pills = []
-        ghosts = []
-        level += 1
-        levelSpan.innerHTML = level
-        score = 0
-        scoreToBeat = 0
-        player.x = 30
-        player.y = 30
-        createPillGrid(pillsObj)
-        drawPills(ctx, pills)
-        createGhostObjs()
-    }
+
 
     let characters = new Image()
     characters.src = './assets/pacman.png'
@@ -254,10 +273,26 @@ const render = () => {
     // Draw Ghosts
     ghosts.map(ghost => {
         ctx.drawImage(characters, ghost.ghostCropX, ghost.ghostCropY, ghost.sWidth, ghost.sHeight, ghost.x, ghost.y, ghost.dWidth, ghost.dHeight)
+        ghostMove(ghost)
+        collisionHandler(ghost)
     })
-
     // Draw Eyes
     ctx.drawImage(characters, 380, 65, 32, 32, 627, 250, 50, 50)
+    console.log(scoreToBeat)
+    // Next level
+    if (score === scoreToBeat) {
+        pills = []
+        ghosts = []
+        level += 1
+        levelSpan.innerHTML = level
+        score = 0
+        scoreToBeat = 0
+        player.x = playerX
+        player.y = playerX
+        createPillGrid(pillsObj)
+        drawPills(ctx, pills)
+        createGhostObjs()
+    }
 }
 
 const playGame = () => {
